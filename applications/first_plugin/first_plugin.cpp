9d3bd27e593d657ac626f739d4dc98207b0141c9
Kingwad <Kingwad@users.noreply.github.com>
2022-05-24 13:58:01 -0400

    Minor cleanup

--- applications/first_plugin/first_plugin.cpp
+++ applications/first_plugin/first_plugin.cpp
@@ -1,5 +1,13 @@
 #include "first_plugin.h"
-//#include <stdlib.h>
+
+// app enter function
+extern "C" int32_t first_plugin_app(void* _args) {
+	FirstPlugin* app = new FirstPlugin();
+	int32_t error = app->Run(_args);
+	delete app;
+
+	return error;
+}
 
 FirstPlugin::FirstPlugin()
 	: m_initialized(false),
