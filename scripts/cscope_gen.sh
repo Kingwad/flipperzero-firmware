c854cc627991b854dacd683593993a5f2c5aeee1
Kingwad <Kingwad@users.noreply.github.com>
2022-07-15 12:05:11 -0400

    Remove symlinks from cscope files

--- scripts/cscope_gen.sh
+++ scripts/cscope_gen.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-find . -name '*.py' \
+find . -type f -a \
+        \( -name '*.py' \
 	-o -name '*.cpp' \
 	-o -name '*.cc' \
 	-o -name '*.c' \
 	-o -name '*.hpp' \
 	-o -name '*.h' \
-	-o -name '*.asm' \
+	-o -name '*.asm' \) \
 	> cscope.files
 
 # -b: just build
